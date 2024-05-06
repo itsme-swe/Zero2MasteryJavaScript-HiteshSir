df85027a778891dc03d9799c227f056314d0007f
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-06 18:22:55 +0530

    up

---
+++ 05_Arrays/1_arrays.js
@@ -0,0 +1,17 @@
+const myArray = [2, 4, 6, 8, 10]            // Declaring array in traditional way
+
+console.log(typeof(myArray));   // Object
+
+
+const newArray = new Array('Superman', 'Spiderman', 'Ironman', 'Hulk')      // Declaring array using new keyword
+console.log(newArray);
+// Output: [ 'Superman', 'Spiderman', 'Ironman', 'Hulk' ]
+
+
+console.log(myArray[2]);        // Accessing values in array 
+// Output: 6
+
+console.log(newArray[3]);
+// Output: Hulk
+
+
