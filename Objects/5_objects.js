7dd00b04e01041926f0171b1cbc5743bfa470f8c
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-07 15:38:21 +0530

    up

---
+++ Objects/5_objects.js
@@ -0,0 +1,15 @@
+/********◽ Destructuring Object **********/
+
+const carSpecs = {
+    carname: "Aston Martin",
+    carseries: "DB12",
+    cartype: "sedan",
+    carengine: "V8"
+}
+
+const {carname, cartype} = carSpecs
+
+console.log(carname);
+console.log(cartype);
+
+
