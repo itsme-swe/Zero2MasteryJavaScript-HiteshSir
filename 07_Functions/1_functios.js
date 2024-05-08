addc10d1fd4dd0618c7c3be5e82156271637d0c7
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-08 07:17:50 +0530

    update

---
+++ 07_Functions/1_functios.js
@@ -0,0 +1,28 @@
+// Creating function
+
+function addTwonumber(num1, num2){      // Passing values while defining function is know as parameters
+    console.log((num1 + num2));
+     
+    
+}
+
+const result = addTwonumber(5, 5)        // Passing values while calling functions is know as arguments
+// Output: 10
+
+console.log(result);
+// Output: undefined
+
+
+/***************************************************************/
+
+function sumOfNum(num1, num2) {
+    return num1 + num2
+}
+
+const finalResult = sumOfNum(50, 10)
+console.log(finalResult);
+// Output: 60
+
+
+/***************************************************************/
+
