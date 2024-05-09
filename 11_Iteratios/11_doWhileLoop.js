65f1bb65a450ee3d2bcc699e26c3e589d952ea08
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-09 13:09:55 +0530

    update

---
+++ 11_Iteratios/11_doWhileLoop.js
@@ -0,0 +1,9 @@
+/*
+◽ Do While Loop
+*/
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
---
+++ 11_Iteratios/2_whileLoop.js
@@ -0,0 +1,16 @@
+/*
+◽ While Loops 
+*/
+
+// Priting counting using while loop
+
+let count = 1;
+let result = ""
+while (count <= 10) {
+    result += count + " "
+    count++
+    
+}
+console.log(result);
+
+// We took result variable just to print the counting i one line 
