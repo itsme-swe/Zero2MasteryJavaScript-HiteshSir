b2029f980bd91e671a66f4b6ca8b7a75fbd71f72
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-09 20:15:56 +0530

    up

---
+++ HigherOrderLoops/5_filter_Method.js
@@ -0,0 +1,11 @@
+/*
+
+◽ Filter() method used to filter out elements based on certain conditions. The difference between filter() and forEach() function is filter() return new array to us and forEach() didnot return any value to us. .🔸filter( () => {} ) method also takes callBack fuction as parameter. And we need to store filter method in some new variable.
+
+*/
+
+const arr = [2, 10, 5, 8, 4, 30, 60, 50 ]
+
+const value = arr.filter( (item) => item > 10 )         // condition is value of item should be greater than 10
+
+console.log(value);     // Output: [ 30, 60, 50 ]
