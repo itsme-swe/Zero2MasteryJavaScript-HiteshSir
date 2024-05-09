7a55983c73fcb626cec227b850ac78817d60e908
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-09 08:39:47 +0530

    update

--- 11_Iteratios/1_forLoop.js
+++ 11_Iteratios/1_forLoop.js
@@ -7,3 +7,15 @@ for (let i = 1; i <= 10; i++) {
     const element = i
     console.log(element)
 }
+
+console.log();
+
+/********** Array **********/
+
+let myArr = ['flash', 'batman', 'superman']
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element)
+    
+}
